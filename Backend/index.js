a5c573adf10b1536243541b02e4d8ef161bc6902
Tushar106 <tusharbhatia106@gmail.com>
2024-11-07 22:57:03 +0530

    added cors and mongo to backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,34 @@
-const express=require('express')
-const app=express();
-app.get("/",(req,res)=>{
+require('dotenv').config();
+const express = require('express')
+const mongoose = require('mongoose')
+const cors=require('cors')
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const connect = async () => {
+    try {
+        mongoose.set("strictQuery", false);
+        await mongoose.connect('mongodb://localhost:27017/tambola')
+        console.log('Connected to database');
+    } catch (error) {
+        throw error;
+    }
+}
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose connected');
+})
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconnected');
+})
+mongoose.connection.on('error', (error) => {
+    console.log('Mongoose connection error', error);
+})
+app.get("/", (req, res) => {
     res.send("Server Working")
 })
-app.listen(8800,()=>{
+app.listen(8800, () => {
+    connect();
     console.log('Server is running on port 8800');
 })
